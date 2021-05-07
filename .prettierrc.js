9ea3840d821148c8fc82a1f5e55511ecf8dc8454
abdo643 <abou.shehata643@yahoo.de>
2021-05-08 01:21:32 +0200

    added configs

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+	bracketSpacing: true,
+	printWidth: 80,
+	semi: true,
+	singleQuote: true,
+	tabWidth: 4,
+	trailingComma: 'all',
+	useTabs: true,
+};
--- rollup.config.cjs
+++ rollup.config.cjs
@@ -3,6 +3,8 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import svelte from 'rollup-plugin-svelte';
 import css from 'rollup-plugin-css-only';
+import livereload from 'rollup-plugin-livereload';
+
 import { terser } from 'rollup-plugin-terser';
 import sveltePreprocess from 'svelte-preprocess';
 import pkg from './package.json';
@@ -21,16 +23,49 @@ export default {
 		{ file: pkg.main, format: 'umd', name },
 	],
 	plugins: [
+		typescript({ sourceMap: !production }),
 		svelte({
+			compilerOptions: {
+				// enable run-time checks when not in production
+				dev: !production,
+			},
 			preprocess: sveltePreprocess(),
 		}),
 		css({ output: 'bundle.css' }),
-		typescript({ sourceMap: !production }),
 		resolve(),
 		commonjs(),
+		!production && serve(),
+		// Watch the `public` directory and refresh the
+		// browser on changes when not in production
+		!production && livereload('public'),
 		production && terser(),
 	],
 	watch: {
 		clearScreen: false,
 	},
 };
+
+function serve() {
+	let server;
+
+	function toExit() {
+		if (server) server.kill(0);
+	}
+
+	return {
+		writeBundle() {
+			if (server) return;
+			server = require('child_process').spawn(
+				'npm',
+				['run', 'start', '--', '--dev'],
+				{
+					stdio: ['ignore', 'inherit', 'inherit'],
+					shell: true,
+				},
+			);
+
+			process.on('SIGTERM', toExit);
+			process.on('exit', toExit);
+		},
+	};
+}
